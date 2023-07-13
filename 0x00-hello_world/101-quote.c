2c0c1ae3185e72a344ba8a78bd59f9cc2b578a26
Blair103 <tonnyblair2000@gmail.com>
2023-07-13 14:28:48 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unstd.h>
+/**
+ * main - C program that prints a given message
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}

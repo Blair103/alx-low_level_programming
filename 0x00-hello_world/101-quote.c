e6f57fded76771826f403e3238f03c2c3ca1c78a
Blair103 <tonnyblair2000@gmail.com>
2023-07-15 10:53:51 -0700

    Task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
-#include <unstd.h>
+#include <unistd.h>
+
 /**
- * main - C program that prints a given message
+ * main - prints "and that piece of art is useful"
  * Return: 1 (success)
  */
+
 int main(void)
+
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" -Dora kopar, 2015-10-19\n", 59);
 	return (1);
 }
